2a47ea607bd0d54359db7c9892611e169a4bbdb5
Med Aziz <zizou.sbl86@gmail.com>
2022-11-29 21:51:09 +0000

    version sab

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -5,13 +5,14 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
 class LuckyController extends AbstractController 
 {
+    #[Route('/lucky/number', name: 'app_index')]
     public function number(): Response
     {
-        $number = random_int(0, 100);
         return $this->render('lucky/index.html.twig', [
-            'number' => $number,
+            
         ]);
     }
 }
