29c4d254e2d6b7638d946ef24a5e3536d1e751bc
Med Aziz <zizou.sbl86@gmail.com>
2022-11-30 19:47:28 +0000

    version mercredi

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -35,7 +35,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_profile');
+            return $this->redirectToRoute('app_login    ');
         }
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
