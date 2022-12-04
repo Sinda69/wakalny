0dd32120493ce50764c99454e3db6be49cbd9a00
Med Aziz <zizou.sbl86@gmail.com>
2022-12-04 14:01:02 +0000

    editt

--- src/Controller/EditProfileController.php
+++ src/Controller/EditProfileController.php
@@ -8,26 +8,13 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-<<<<<<< HEAD
-  
+
 class EditProfileController extends AbstractController
 {
      #[Route('/edit' , name:'app_edit')]
+  public function editProfile(Request $request,PersistenceManagerRegistry $doctrine)
 
-    public function editProfile(Request $request,PersistenceManagerRegistry $doctrine)
-=======
-use Dompdf\Dompdf;
-use Dompdf\Options;
-use Symfony\Component\HttpFoundation\Response;
-
-   
-     
-class EditProfileController extends AbstractController
-{
-    #[Route('/profile', name: 'app_edit')]
-    public function editProfile(Request $request)
->>>>>>> b8c47390e1aa71386849a6e7fd9fc854e8a8bb66
-    {
+  {
         $user = $this->getUser();
         $form = $this->createForm(EditProfileType::class, $user);
 
